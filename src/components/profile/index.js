f461161584aec8d833332ffb5047f672a5b26c50
robson <robsonfs.sp@hotmail.com>
2022-06-16 22:30:11 -0300

    desacoplamento statusCount

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import useGithub from "../../hooks/github-hooks";
 import * as S from "./styled";
+import StatusCount from "../statusCount";
 
 const Profile = () => {
   const { githubState } = useGithub();
@@ -36,24 +37,10 @@ const Profile = () => {
             </a>
           </S.WrapperUserGeneric>
         </div>
-        <S.WrapperStatusCount>
-          <div>
-            <h4>Followers</h4>
-            <span> {githubState.user.followers}</span>
-          </div>
-          <div>
-            <h4>Followings</h4>
-            <span> {githubState.user.following}</span>
-          </div>
-          <div>
-            <h4>Gists</h4>
-            <span> {githubState.user.public_gists}</span>
-          </div>
-          <div>
-            <h4>Repos</h4>
-            <span> {githubState.user.public_repos}</span>
-          </div>
-        </S.WrapperStatusCount>
+        <div>
+          <StatusCount />
+        </div>
+        
       </S.WrapperInfoUser>
     </S.Wrapper>
   );
